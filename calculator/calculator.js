41ec39963c3c081bc006b546aba92fc3e7ed2fe6
Nihal Ahmed <nihaltomail@gmail.com>
2021-12-13 15:02:41 +0530

    Test cases

---
+++ calculator/calculator.js
@@ -0,0 +1,27 @@
+const add = (x, y) => {
+
+    return +x + +y;
+
+}
+
+const sub = (x, y) => {
+
+    return +x - +y;
+
+}
+
+const multi = (x, y) => {
+
+    return +x * +y;
+
+}
+
+const divide = (x, y) => {
+
+    return +x / +y;
+
+}
+
+
+module.exports = { add, sub, multi, divide };
+
